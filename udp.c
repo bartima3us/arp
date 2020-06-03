aa6dd715969609ae0c2ab408ae1252bdc20d4f15
Šarūnas <sarunas0919@gmail.com>
2020-06-03 22:22:06 +0300

    Payload parsing fix.

--- udp.c
+++ udp.c
@@ -11,11 +11,12 @@ void handle_udp(char* buffer, struct ether_header recv_ether_dgram, struct iphdr
 {
     struct udphdr recv_udp_header;
 
-    recv_udp_header = *(struct udphdr*)&buffer[sizeof(recv_ether_dgram) + sizeof(recv_ipv4_header)];
-    int payload_size = htons(recv_udp_header.len) - 8;
+    int ether_ip_size = sizeof(recv_ether_dgram) + sizeof(recv_ipv4_header);
+    recv_udp_header = *(struct udphdr*)&buffer[ether_ip_size];
+    int payload_size = htons(recv_udp_header.len) - sizeof(struct udphdr);
     char payload[payload_size];
 
-    memcpy(payload, &buffer[sizeof(recv_udp_header)], (size_t)payload_size);
+    memcpy(payload, &buffer[ether_ip_size + sizeof(struct udphdr)], (size_t)payload_size);
 
     printf("UDP. Src port: %i, dst port: %i, pld: %s\n", htons(recv_udp_header.source), htons(recv_udp_header.dest), payload);
 }
